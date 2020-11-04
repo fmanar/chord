39ffbd39c9a7d4411a4126a8aabc8983116a903d
Field <field.manar@gmail.com>
2020-11-04 14:14:56 -0500

    full testing for pitch and bugfix

--- chord/pitch.py
+++ chord/pitch.py
@@ -1,16 +1,25 @@
+import functools
 import re
 
+@functools.total_ordering
 class Pitch:
     '''Stores a single pitch.
 
-    Pitch is represented as an integer number of half-steps above a reference.
-    The absolute scale corresponds the MIDI tuning standard with C4=60.
-    Support for addition and subtraction is provided.  Attributes are 
-    erased if two notes have disimilar values.
+    Pitch is represented as an integer number of half-steps above a
+    reference. Support is provided to name the pitches by a letter, a sharp
+    (#) or flat (b) and an optional octave number (corresponds to the MIDI
+    tuning standard with C4=60). As a string, this would be something like
+    'F#4'. Support for addition and subtraction is provided, and works with
+    integers. Attributes are erased if two pitches have disimilar
+    dictionaries.
 
     Attributes:
         pitch (int): the pitch
         attrib (dict): dictionary of additional attributes
+
+    Examples:
+        >>> p = Pitch(60) # a middle C
+        >>> p = Pitch('C4') # also a middle C
 
     '''
     def __init__(self, value, attrib={}):
@@ -19,21 +28,34 @@ class Pitch:
         elif isinstance(value, str):
             self.pitch = 0
             self.set_name(value)
+        else:
+            raise ValueError
         self.attrib = attrib
 
     @classmethod
     def make(cls, value):
-        '''A factory method.'''
+        '''A factory method to allow easy casting.'''
         if isinstance(value, cls):
             return value
         else:
             return Pitch(value)
-    
+
+    def get_octave(self):
+        '''Return MIDI octave number.'''
+        return (self.pitch // 12) - 1
+
+    def get_class(self):
+        '''Return pitch modulo 12'''
+        return self.pitch % 12
+
     def get_name(self, absolute=False, flats=False):
-        names_sharp = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B']
-        names_flat  = ['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
-        octave = (self.pitch // 12) - 1
-        pitch = self.pitch % 12
+        '''Return letter name for pitch.'''
+        names_sharp = ['C', 'C#', 'D', 'D#', 'E', 'F', 
+                'F#', 'G', 'G#', 'A', 'A#', 'B']
+        names_flat  = ['C', 'Db', 'D', 'Eb', 'E', 'F', 
+                'Gb', 'G', 'Ab', 'A', 'Bb', 'B']
+        octave = self.get_octave()
+        pitch = self.get_class()
 
         if flats:
             name = names_flat[pitch]
@@ -46,9 +68,10 @@ class Pitch:
         return name
 
     def set_name(self, name):
-        d = {'c':0, 'c#':1, 'db':1, 'd':2, 'd#':3, 'eb':3, 'e':4, 'f':5,
-                'f#':6, 'gb':6, 'g':7, 'g#':8, 'ab':8, 'a':9, 'a#':10, 'bb':10,
-                'b':11,
+        '''Deduce pitch from letter name.'''
+        d = {'c':0, 'c#':1, 'db':1, 'd':2, 'd#':3, 'eb':3, 
+                'e':4, 'f':5, 'f#':6, 'gb':6, 'g':7, 'g#':8, 
+                'ab':8, 'a':9, 'a#':10, 'bb':10, 'b':11,
         }
         match = re.match('([a-zA-Z][#b]?)(\d*)', name)
         if match:
@@ -63,6 +86,16 @@ class Pitch:
     
     def __repr__(self):
         return f'Pitch({self.pitch}, {self.attrib})'
+
+    def __eq__(self, other):
+        if not isinstance(other, Pitch):
+            other = Pitch(other)
+        return self.pitch == other.pitch
+
+    def __lt__(self, other):
+        if not isinstance(other, Pitch):
+            other = Pitch(other)
+        return self.pitch < other.pitch
 
     def __add__(self, other):
         if isinstance(other, Pitch):
@@ -84,20 +117,24 @@ class Pitch:
         return self + other
 
     def __rsub__(self, other):
-        return self - other
+        return Pitch.make(other) - self
 
     def __iadd__(self, other):
         if isinstance(other, Pitch):
             self.pitch += other.pitch
+            return self
         elif isinstance(other, int):
             self.pitch += other
+            return self
         else:
             return NotImplemented
         
     def __isub__(self, other):
         if isinstance(other, Pitch):
             self.pitch -= other.pitch
+            return self
         elif isinstance(other, int):
             self.pitch -= other
+            return self
         else:
             return NotImplemented
--- test/test_pitch.py
+++ test/test_pitch.py
@@ -4,10 +4,69 @@ from .context import chord
 import chord.pitch
 
 class TestPitch(unittest.TestCase):
-    def test_constructor_int(self):
+    def test_constructor(self):
         p = chord.pitch.Pitch(5)
         self.assertEqual(p.pitch, 5)
 
-    def test_constructor_str(self):
         p = chord.pitch.Pitch("G#4")
         self.assertEqual(p.pitch, 68)
+
+    def test_make(self):
+        p1 = chord.pitch.Pitch(7)
+        p2 = chord.pitch.Pitch.make(p1)
+        self.assertIs(p1, p2)
+
+        p3 = chord.pitch.Pitch.make(7)
+        self.assertEquals(p3.pitch, 7)
+
+    def test_get_name(self):
+        p = chord.pitch.Pitch(61)
+        self.assertEqual(p.get_name(), 'C#')
+        self.assertEqual(p.get_name(absolute=True), 'C#4')
+        self.assertEqual(p.get_name(flats=True), 'Db')
+
+    def test_set_name(self):
+        p = chord.pitch.Pitch(0)
+        p.set_name('Ab7')
+        self.assertEqual(p.pitch, 104)
+
+    def test_compare(self):
+        p1 = chord.pitch.Pitch(60)
+        p2 = chord.pitch.Pitch(60)
+        self.assertEqual(p1, p2)
+        self.assertEqual(p1, 60)
+        self.assertEqual(p1, 'C4')
+
+        p2 = chord.pitch.Pitch(48)
+        self.assertGreater(p1, p2)
+        self.assertGreater(p1, 48)
+        self.assertGreater(p1, 'C3')
+
+        p2 = chord.pitch.Pitch(72)
+        self.assertLess(p1, p2)
+        self.assertLess(p1, 72)
+        self.assertLess(p1, 'C5')
+
+    def test_arithmetic(self):
+        p1 = chord.pitch.Pitch(10)
+        p2 = chord.pitch.Pitch(7)
+        a = p1 + p2
+        b = p1 + 7
+        self.assertEqual(a.pitch, 17)
+        self.assertEqual(b.pitch, 17)
+
+        a = p1 - p2
+        b = p1 - 7
+        self.assertEqual(a.pitch, 3)
+        self.assertEqual(b.pitch, 3)
+        
+        a = 7 + p1
+        b = 7 - p1
+        self.assertEqual(a.pitch, 17)
+        self.assertEqual(b.pitch, -3)
+
+        p1 += 3
+        self.assertEqual(p1.pitch, 13)
+
+        p1 -= 5
+        self.assertEqual(p1, 8)
